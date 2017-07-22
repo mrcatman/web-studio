95904d92d383e8c8b3c65a936d082af66ed206cf
Kirill Alexeev <kittenizator@yandex.ru>
2017-07-23 01:22:23 +0700

    Add employees list

--- index.php
+++ index.php
@@ -168,3 +168,183 @@
   <script src="http://code.jquery.com/jquery-3.1.1.min.js"></script>
   </body>
 </html>
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+	<div class="header">
+		<div class="container">
+			<div class="header__logo"></div>
+			<div class="header__text">web-студия</div>
+		</div>
+	</div>
+	<div class="top-menu">
+		<div class="container">
+			<div class="top-menu__holder">
+				<a href="#" class="top-menu__item top-menu__item_selected">Главная</a>
+				<a href="#" class="top-menu__item">О нас</a>
+				<a href="#" class="top-menu__item">Почему мы?</a>
+				<a href="#" class="top-menu__item">Наши проекты</a>
+				<a href="#" class="top-menu__item">Наша команда</a>
+				<a href="#" class="top-menu__item">Контакты</a>
+			</div>
+		</div>
+	</div>
+	<div class="carousel">
+		<div class="carousel__background" style="background-image:url(http://placekitten.com/1280/720)"></div>
+		<div class="carousel__title">
+			<div class="container">
+				<h1>Верхняя строчка слогана</h1>
+				<h2>Нижняя строчка слогана</h2>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="about">
+			<p class="about__header">Кто мы?</p>
+			<p class="about__text">Мы, веб-студия E-Create, создаём различные веб-проекты с индивидуальным дизайном, разрабатываем промо- и корпоративные сайты, интернет-магазины и многое другое.</p>
+			<p class="about__header">Почему именно мы?</p>
+			<p class="about__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+			<p class="about__header">Что мы делаем?</p>
+		</div>
+		<div class="services">
+			<div class="services__column">
+				<div class="service">
+					<div class="service__icon" style="background-image:url(images/work-icon1.png)"></div>
+					<div class="service__info">
+						<p class="service__name">Веб-дизайн и разработка</p>
+						<p class="service__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+					</div>
+				</div>
+				<div class="service">
+					<div class="service__icon" style="background-image:url(images/work-icon3.png)"></div>
+					<div class="service__info">
+						<p class="service__name">Автоматизация бизнеса</p>
+						<p class="service__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+					</div>
+				</div>
+				<div class="service">
+					<div class="service__icon" style="background-image:url(images/work-icon4.png)"></div>
+					<div class="service__info">
+						<p class="service__name">Брендинг</p>
+						<p class="service__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+					</div>
+				</div>
+			</div>
+			<div class="services__divider"></div>
+			<div class="services__column">
+				<div class="service">
+					<div class="service__icon" style="background-image:url(images/work-icon2.png)"></div>
+					<div class="service__info">
+						<p class="service__name">Мобильные приложения</p>
+						<p class="service__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+					</div>
+				</div>
+				<div class="service">
+					<div class="service__icon" style="background-image:url(images/work-icon4.png)"></div>
+					<div class="service__info">
+						<p class="service__name">Поддержка сайтов</p>
+						<p class="service__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+					</div>
+				</div>
+				<div class="service">
+					<div class="service__icon" style="background-image:url(images/work-icon6.png)"></div>
+					<div class="service__info">
+						<p class="service__name">SEO-продвижение сайтов</p>
+						<p class="service__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="works">
+		<div class="container">
+			<p class="works__header">Наши работы</p>
+			<div class="works__menu">
+				<a class="works__menu__item">Все</a>
+				<a class="works__menu__item">Брендинг</a>
+				<a class="works__menu__item works__menu__item_selected">Веб-дизайн</a>
+				<a class="works__menu__item">Интерфейсы</a>
+				<a class="works__menu__item">Другое</a>
+			</div>
+			<div class="works__container">
+				<div class="works__row">
+					<div class="work">
+						<div class="work__photo" style="background-image:url(http://placekitten.com/640/480)"></div>
+						<p class="work__title">Название работы</p>
+					</div>
+					<div class="work">
+						<div class="work__photo" style="background-image:url(http://placekitten.com/640/480)"></div>
+						<p class="work__title">Название работы</p>
+					</div>
+					<div class="work">
+						<div class="work__photo" style="background-image:url(http://placekitten.com/640/480)"></div>
+						<p class="work__title">Название работы</p>
+					</div>
+					<div class="work">
+						<div class="work__photo" style="background-image:url(http://placekitten.com/640/480)"></div>
+						<p class="work__title">Название работы</p>
+					</div>
+				</div>
+				<div class="works__row">
+					<div class="work">
+						<div class="work__photo" style="background-image:url(http://placekitten.com/640/480)"></div>
+						<p class="work__title">Название работы</p>
+					</div>
+					<div class="work">
+						<div class="work__photo" style="background-image:url(http://placekitten.com/640/480)"></div>
+						<p class="work__title">Название работы</p>
+					</div>
+					<div class="work">
+						<div class="work__photo" style="background-image:url(http://placekitten.com/640/480)"></div>
+						<p class="work__title">Название работы</p>
+					</div>
+					<div class="work">
+						<div class="work__photo" style="background-image:url(http://placekitten.com/640/480)"></div>
+						<p class="work__title">Название работы</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="employees">
+		<div class="container">
+			<p class="employees__title">Наши сотрудники</p>
+			<div class="employees__container">
+				<div class="employees__icon"><i class="ion-chevron-left"></i></div>
+				<div class="employees__list">
+					<div class="employee">
+						<div class="employee__photo" style="background-image:url(http://placekitten.com/640/480)"></div>
+						<p class="employee__name">Сотрудник 1</p>
+						<p class="employee__position">web-разработчик</p>
+					</div>
+					<div class="employee">
+						<div class="employee__photo" style="background-image:url(http://placekitten.com/640/480)"></div>
+						<p class="employee__name">Сотрудник 2</p>
+						<p class="employee__position">web-разработчик</p>
+					</div>
+					<div class="employee">
+						<div class="employee__photo" style="background-image:url(http://placekitten.com/640/480)"></div>
+						<p class="employee__name">Сотрудник 3</p>
+						<p class="employee__position">web-разработчик</p>
+					</div>
+					<div class="employee">
+						<div class="employee__photo" style="background-image:url(http://placekitten.com/640/480)"></div>
+						<p class="employee__name">Сотрудник 4</p>
+						<p class="employee__position">web-разработчик</p>
+					</div>
+				</div>
+				<div class="employees__icon"><i class="ion-chevron-right"></i></div>
+			</div>
+		</div>   
+	</div>
+  <link rel="stylesheet" href="css/main.css?<?=time()?>">
+  <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700,900" rel="stylesheet">
+  <link href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet">
+  <script src="http://code.jquery.com/jquery-3.1.1.min.js"></script>
+  </body>
+</html>
